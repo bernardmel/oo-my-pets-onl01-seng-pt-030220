c6e12c1accdf2ebdc83871622b5c60a8bb08575e
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-30 19:26:59 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,16 +3,17 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
 
-  @@all = []
+
 
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all = []
   end
 
   def self.all
-    @@all 
+    @@all
   end
 
 end
