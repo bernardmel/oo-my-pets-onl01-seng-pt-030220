fb282c8b6ae318486e11a8b5c8a51f82678e1014
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-30 18:33:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,9 +2,16 @@ class Owner
   # code goes here
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+
   end
 
+  def self.all 
+    @@all
+  end
 
 end
